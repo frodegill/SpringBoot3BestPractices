8cfe7efacd9dfd47014b663941ba11e5cf1ae691
Frode Roxrud Gill <frodegill@gmail.com>
2024-01-15 17:22:46 +0100

    Let Swagger UI display application name and version from pom.xml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,27 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	private BuildProperties buildProperties;
+
+	@Bean
+	OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title(buildProperties.getArtifact())
+				.version(buildProperties.getVersion()));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
